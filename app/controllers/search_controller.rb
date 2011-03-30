d7e38c77e8b24fd0c6323cf83643013bd85a2b1b
Chris Stump <chris@tablexi.com>
2011-03-30 14:57:50 -0500

    Task #30165: revert back to nucore-open code; move NU/bcsec logic to extensions

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,13 +10,12 @@ class SearchController < ApplicationController
     @price_group = PriceGroup.find(params[:price_group_id]) if params[:price_group_id]
     @account     = Account.find(params[:account_id]) if params[:account_id]
     @product     = Product.find(params[:product_id]) if params[:product_id]
-    
+
     term = generate_multipart_like_search_term(params[:search_term])
     if params[:search_term].length > 0
-      conditions = ["(LOWER(t_personnel.first_name) LIKE ? OR LOWER(t_personnel.last_name) LIKE ? OR LOWER(t_personnel.username) LIKE ? OR LOWER(CONCAT(t_personnel.first_name, t_personnel.last_name)) LIKE ?) AND t_security_logins.portal = ?",
-                    term, term, term, term, NUCore.portal]
-      @users = Pers::Person.find(:all, :conditions => conditions, :order => "last_name, first_name", :joins => 'INNER JOIN t_security_logins ON t_security_logins.username = t_personnel.username', :limit => @limit)
-      @count = Pers::Person.count(:all, :conditions => conditions, :joins => 'INNER JOIN t_security_logins ON t_security_logins.username = t_personnel.username')
+      conditions = ["(LOWER(first_name) LIKE ? OR LOWER(last_name) LIKE ? OR LOWER(username) LIKE ? OR LOWER(CONCAT(first_name, last_name)) LIKE ?)", term, term, term, term]
+      @users = User.find(:all, :conditions => conditions, :order => "last_name, first_name", :limit => @limit)
+      @count = User.count(:all, :conditions => conditions)
     end
     render :layout => false
   end
