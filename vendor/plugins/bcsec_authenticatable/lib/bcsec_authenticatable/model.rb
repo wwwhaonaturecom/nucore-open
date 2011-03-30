0c4981306fa51d7ffe29e87544bd294c6c62ba0f
Chris Stump <chris@tablexi.com>
2011-03-30 15:39:09 -0500

    Task #30165: move non-auth code from bcsec_authenticatable model mixin to extension

--- vendor/plugins/bcsec_authenticatable/lib/bcsec_authenticatable/model.rb
+++ vendor/plugins/bcsec_authenticatable/lib/bcsec_authenticatable/model.rb
@@ -6,29 +6,6 @@ module Devise
         base.class_eval { attr_accessor :password }
       end
 
-
-      def after_create
-        raise 'could not create pers record' unless Pers::Person.find_or_create_by_username({
-          :first_name => first_name,
-          :last_name => last_name,
-          :email => email,
-          :username => username,
-          :entered_date => Time.zone.now,
-          :plain_text_password => password
-        })
-      end
-
-
-      def after_save
-        ensure_login_record_exists
-      end
-
-
-      def ensure_login_record_exists
-        login = Pers::Login.first(:conditions => { :portal => 'nucore', :username => username })
-        Pers::Login.create(:portal_name => 'nucore', :username => username) unless login
-      end
-
     end
   end
 end
