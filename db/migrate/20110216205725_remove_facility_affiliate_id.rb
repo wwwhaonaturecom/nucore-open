3a2cd651862ee3cb87ad72f708cb5ee4ff5653c1
Chris Stump <chris@tablexi.com>
2011-04-01 11:26:53 -0500

    don't let this column drop until after NU user migration

--- db/migrate/20110216205725_remove_facility_affiliate_id.rb
+++ db/migrate/20110216205725_remove_facility_affiliate_id.rb
@@ -1,9 +1,11 @@
 class RemoveFacilityAffiliateId < ActiveRecord::Migration
   def self.up
-    remove_column(:facilities, :pers_affiliate_id)
+    #remove_column(:facilities, :pers_affiliate_id)
+    puts ">>>>>>> YOU SHOULD DROP THE facilities.pers_affiliate_id column AFTER MIGRATING NU USERS! <<<<<<<"
   end
 
   def self.down
-    add_column(:facilities, :pers_affiliate_id, :integer)
+    #add_column(:facilities, :pers_affiliate_id, :integer)
+    puts ">>>>>>> IF YOU DROPPED THE facilities.pers_affiliate_id column AFTER MIGRATING NU USERS, PUT IT BACK! <<<<<<<"
   end
 end
