f6bd03b4c840e68824e8dbc3766936347912fff0
John Gore <john@tablexi.com>
2011-05-24 12:58:58 -0500

    Commented out MySQL adapter settings.  Force oracle.

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -3,15 +3,17 @@
 File.open("#{Rails.root}/config/database.yml") do |yml|
   config=YAML.load(yml)
 
-  if config[Rails.env]['adapter'] == 'oracle_enhanced'
+  ## Commented out as production nucore uses Bcdatabase to populate database.yml
+  ## so the database.yml file is NOT parseable via normal strategies.
+  #if config[Rails.env]['adapter'] == 'oracle_enhanced'
     ActiveRecord::ConnectionAdapters::OracleEnhancedAdapter.class_eval do
       self.emulate_integers_by_column_name = true
       self.default_sequence_start_value = 1
     end
-  else
-    ActiveRecord::ConnectionAdapters::MysqlAdapter.class_eval do
-      require File.join(Rails.root, 'lib', 'mysql_driver_extension')
-      include MysqlDriverExtension
-    end
-  end
+  #else
+  #  ActiveRecord::ConnectionAdapters::MysqlAdapter.class_eval do
+  #    require File.join(Rails.root, 'lib', 'mysql_driver_extension')
+  #    include MysqlDriverExtension
+  #  end
+  #end
 end
