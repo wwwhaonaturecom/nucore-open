9da65a245ef830549d889ab82840c15642a1633f
Chris Stump <chris@tablexi.com>
2011-06-30 15:55:57 -0500

    overwrite #price_groups

--- lib/extensions/user_extension.rb
+++ lib/extensions/user_extension.rb
@@ -1,5 +1,19 @@
 module UserExtension
 
+  def price_groups
+    groups = price_group_members.collect{ |pgm| pgm.price_group }
+    # check internal/external membership
+    groups << (self.username.match(/@/) ? PriceGroup.external.first : PriceGroup.northwestern.first)
+    # check cancer center membership
+    begin
+      result = Pers::Person.find_by_sql(["SELECT * from v_cancer_center_members where username = ?", self.username])
+      groups << PriceGroup.cancer_center.first if result.length > 0
+    rescue
+    end
+    groups.flatten.uniq
+  end
+
+
   def after_create
     raise 'could not create pers record' unless Pers::Person.find_or_create_by_username({
       :first_name => first_name,
