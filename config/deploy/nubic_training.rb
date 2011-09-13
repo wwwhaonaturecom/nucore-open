2cd46bb76debdfc0bb7911e344d6cbf3412a7309
Chris Stump <chris@tablexi.com>
2011-09-13 14:47:35 -0500

    Task #35969 : system reports chart string expired though it has a 2012 exp date in GL066

--- config/deploy/nubic_training.rb
+++ config/deploy/nubic_training.rb
@@ -29,6 +29,7 @@ namespace :deploy do
     run "ln -s #{deploy_to}/database.yml #{release_path}/config/database.yml"
     run "ln -s #{deploy_to}/Constants.rb #{release_path}/config/Constants.rb"
     run "ln -s #{deploy_to}/files #{release_path}/public/files"
+    run "ln -s #{deploy_to}/database.yml #{release_path}/vendor/engines/nucs/config/database.yml"
   end
   task :bundle_install do
     run "cd #{release_path} && ~/.gem/ruby/1.8/bin/./bundle install ../../shared/bundle"
