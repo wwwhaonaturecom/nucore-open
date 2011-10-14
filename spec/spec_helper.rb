926a831b5e66e87debc597a9a0a15ac87a0f4d1c
Chris Stump <chris@tablexi.com>
2011-10-14 16:31:18 -0500

    Move bcaudit overwrite to spec_helper so users create and tests pass

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -89,6 +89,24 @@ Spork.prefork do
       @ccpg.save(:validate => false)
       @epg = PriceGroup.find_or_create_by_name(:name => 'External Rate', :is_internal => false, :display_order => 3)
       @epg.save(:validate => false)
+
+      # bcsec's ensuring to satisfy bcaudit on save is
+      # handled in nucore by Bcaudit::Middleware which is
+      # loaded at bcsec_authenticatable's initialization.
+      # Since it's rack middleware, and therefore depends
+      # on being a request-response cycle, it doesn't work
+      # for tests. Ideally there would be a bcsec-provided
+      # mock to handle this, but there isn't, so overwrite
+      # the method that ensures bcaudit satisfaction so that
+      # tests can proceed. Doing so allows the NU user
+      # provisioning that takes place in user_extension to
+      # work, making it testable
+      Pers::Base.class_eval %Q<
+        protected
+
+        def ensure_bcauditable
+        end
+      >
     end
   end
 end
