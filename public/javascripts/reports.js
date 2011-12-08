8d07f3ac5b0e4faf759f4525d84d8985560cb78d
Chris Stump <chris@tablexi.com>
2011-12-08 16:08:38 -0600

    Task #38983 : Improve "export all" feature of reports

--- public/javascripts/reports.js
+++ public/javascripts/reports.js
@@ -63,7 +63,11 @@ function initReportsUI(selectedIndex)
             // every time a tab loads make sure the export urls are set to export current report
             var url=getUpdateTabUrl(ui);
             $('#export').attr('href', url + '&export_id=report&format=csv');
-            //$('#export-all').attr('href', url + '&export_id=report_data&format=csv');
+
+            var all=$('#export-all');
+
+            if(!all.hasClass('disabled'))
+                all.attr('href', url + '&export_id=report_data&format=csv');
         },
 
         ajaxOptions: {
