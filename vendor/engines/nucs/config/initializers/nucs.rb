55c302a5b9ad818c303d9e30132e3566b907e7dd
Chris Stump <chris@tablexi.com>
2012-07-03 15:39:27 -0500

    no more core tests -- they are now in nu engine

--- vendor/engines/nucs/config/initializers/nucs.rb
+++ vendor/engines/nucs/config/initializers/nucs.rb
@@ -5,16 +5,4 @@ if Rails.env.development? || Rails.env.test?
   #
   # Use the host app's config during development
   FileUtils.ln_sf File.expand_path('../../../../../config/database.yml', base), File.expand_path('../database.yml', base)
-
-  #
-  # These specs should be integrated into the main app's spec run
-  %w(models controllers).each do |spec_dir|
-    integrate_path=File.expand_path("../../../../../spec/#{spec_dir}/nucs", base)
-    # must integrate via a real directory (i.e. can't symlink dir) or rspec won't find specs
-    FileUtils.mkdir integrate_path unless File.exists? integrate_path
-
-    Dir[File.expand_path("../../spec/core/#{spec_dir}/*_spec.rb", base)].each do |spec|
-      FileUtils.ln_sf spec, integrate_path
-    end
-  end
 end
