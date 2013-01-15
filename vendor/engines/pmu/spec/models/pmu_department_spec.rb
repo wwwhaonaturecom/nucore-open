fdca8d65062fcaa7fcc884c010c0622742c268b6
Chris Stump <chris@tablexi.com>
2013-01-15 16:02:49 -0600

    specs for model
    Task #48365 : Leverage the PMU (primary management unit) mapping

--- vendor/engines/pmu/spec/models/pmu_department_spec.rb
+++ vendor/engines/pmu/spec/models/pmu_department_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe PmuDepartment do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should have_db_column(:unit_id).of_type :string }
+  it { should have_db_column(:pmu).of_type :string }
+  it { should have_db_column(:area).of_type :string }
+  it { should have_db_column(:division).of_type :string }
+  it { should have_db_column(:organization).of_type :string }
+  it { should have_db_column(:fasis_id).of_type :string }
+  it { should have_db_column(:fasis_description).of_type :string }
+  it { should have_db_column(:nufin_id).of_type :string }
+  it { should have_db_column(:nufin_description).of_type :string }
+
 end
