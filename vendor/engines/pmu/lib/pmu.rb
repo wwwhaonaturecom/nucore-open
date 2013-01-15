650fca13c2023c6d3b3972611db23beed3b7fe2d
Chris Stump <chris@tablexi.com>
2013-01-14 18:28:32 -0600

    autoload lib
    Task #48365 : Leverage the PMU (primary management unit) mapping

--- vendor/engines/pmu/lib/pmu.rb
+++ vendor/engines/pmu/lib/pmu.rb
@@ -1,5 +1,7 @@
 module Pmu
   class Engine < Rails::Engine
+    config.autoload_paths << File.join(File.dirname(__FILE__), "../lib")
+
     config.to_prepare do
       # make this engine's views override the main app's views
       paths=ActionController::Base.view_paths.dup
