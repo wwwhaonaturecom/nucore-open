7a4176d51ebc37c08eca4884e5b74c4ab55d05f1
Jason Hanggi <jason@tablexi.com>
2013-03-27 16:20:42 -0500

    reservation#instrument should be #product

--- vendor/engines/pmu/lib/pmu/instrument_reports_controller_extension.rb
+++ vendor/engines/pmu/lib/pmu/instrument_reports_controller_extension.rb
@@ -4,7 +4,7 @@ module Pmu
     def department
       render_report(8, 'Name') do |res|
         acct = res.order_detail.account
-        acct.respond_to?(:pmu_description) ?  [ res.instrument.name, acct.pmu_description ] : [ res.instrument.name, 'Unknown' ]
+        acct.respond_to?(:pmu_description) ?  [ res.product.name, acct.pmu_description ] : [ res.product.name, 'Unknown' ]
       end
     end
 
