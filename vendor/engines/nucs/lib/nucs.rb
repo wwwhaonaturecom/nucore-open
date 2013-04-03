9627bca3a144800e25a66405776f6e7628b72930
Jason Hanggi <jason@tablexi.com>
2013-04-03 16:28:58 -0500

    validate that revenue accounts start with 4 or 5

--- vendor/engines/nucs/lib/nucs.rb
+++ vendor/engines/nucs/lib/nucs.rb
@@ -1,5 +1,9 @@
 module Nucs
   class Engine < Rails::Engine
     config.autoload_paths << File.join(File.dirname(__FILE__), "../lib")
+
+    config.to_prepare do
+      FacilityAccount.send :include, NucsValidations::FacilityAccount
+    end
   end
 end
