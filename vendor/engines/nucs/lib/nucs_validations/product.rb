cc6ba8c9a63aafbcd318ed3da701f9e6961bb4b8
Jason Hanggi <jason@tablexi.com>
2013-04-05 11:01:22 -0500

    more spec failure fixes due to new expense/revenue account validations

--- vendor/engines/nucs/lib/nucs_validations/product.rb
+++ vendor/engines/nucs/lib/nucs_validations/product.rb
@@ -2,6 +2,9 @@ module NucsValidations::Product
   extend ActiveSupport::Concern
 
   included do
-    validates_format_of :account, :with => /^7/, :message => I18n.t('activerecord.errors.product.account.format')
+    validates_format_of :account,
+                        :with => /^7/,
+                        :message => I18n.t('activerecord.errors.product.account.format'),
+                        :if => lambda { SettingsHelper.feature_on? :expense_accounts }
   end
 end
