6f166d33a8d7f4a0e6a1078a896f7fe5b535cfcf
Jason Hanggi <jason@tablexi.com>
2013-05-13 15:22:39 -0500

    make sure error messages get loaded for account validation when cache_classes=true

--- vendor/engines/nucs/lib/nucs_validations/facility_account.rb
+++ vendor/engines/nucs/lib/nucs_validations/facility_account.rb
@@ -2,6 +2,8 @@ module NucsValidations::FacilityAccount
   extend ActiveSupport::Concern
 
   included do
-    validates_format_of :revenue_account, :with => /^[45]/, :message => I18n.t('activerecord.errors.facility_account.revenue_account.format')
+    validates_format_of :revenue_account,
+        :with => /^[45]/,
+        :message => lambda { I18n.t('activerecord.errors.facility_account.revenue_account.format') }
   end
 end
