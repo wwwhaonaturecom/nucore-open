bc48c384fe2ff4fbfa165e508569633cb53b1f8a
Jason Hanggi <jason@tablexi.com>
2013-08-15 17:29:48 -0500

    change staging host for emails

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -46,7 +46,7 @@ Nucore::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method       = :smtp
-  config.action_mailer.default_url_options   = { :host => "nucore.northwestern.edu", :protocol => 'https' }
+  config.action_mailer.default_url_options   = { :host => "nucore-staging.nubic.northwestern.edu", :protocol => 'https' }
   config.action_mailer.smtp_settings         = {
     :address        => 'ns.northwestern.edu',
     :port           => 25,
