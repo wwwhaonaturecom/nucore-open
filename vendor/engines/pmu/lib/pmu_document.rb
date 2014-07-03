fd1b737c02a3a41a65722ad324b68bf78c6b89f6
Jason Hanggi <jason@tablexi.com>
2014-07-03 18:25:47 -0500

    [#86042] Fix PMU cron error
    Cognos report added new fields. Now ignore any new fields.

--- vendor/engines/pmu/lib/pmu_document.rb
+++ vendor/engines/pmu/lib/pmu_document.rb
@@ -39,7 +39,7 @@ class PmuDocument < Nokogiri::XML::SAX::Document
   def end_element(name)
     case name
       when 'value'
-        @attrs[ @value_sequence[@sequence_ndx] ] = @value
+        @attrs[ @value_sequence[@sequence_ndx] ] = @value if @sequence_ndx < @value_sequence.length
       when 'row'
         if @attrs[:nufin_id].present?
           dept = PmuDepartment.find_or_create_by_nufin_id @attrs[:nufin_id]
