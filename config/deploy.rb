cbcdfecf41b0d0b4c19d4702411faeac7bb196a6
Lloyd Philbrook <lloyd@tablexi.com>
2014-11-07 11:16:20 -0600

    override cap eye:load_config task

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,3 +6,11 @@ require 'rvm/capistrano'
 require "eye/patch/capistrano"
 set :eye_config, "config/eye.yml.erb"
 set(:eye_env) { fetch(:rails_env) }
+
+namespace :eye do
+  desc "Start eye with the desired configuration file"
+  task :load_config, roles: -> { fetch(:eye_roles) } do
+    # force a full shell
+    run "bash -lc 'cd #{current_path} && #{fetch(:eye_bin)} q && #{fetch(:eye_bin)} l #{fetch(:eye_config)}'"
+  end
+end
