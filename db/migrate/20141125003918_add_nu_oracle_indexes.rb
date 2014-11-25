e3a67c1a4ee478473bee3a5ccd9182c5ffbe8e35
Jason Hanggi <jason@tablexi.com>
2014-11-24 19:51:34 -0500

    Add oracle-specific indexes

---
+++ db/migrate/20141125003918_add_nu_oracle_indexes.rb
@@ -0,0 +1,30 @@
+class AddNuOracleIndexes < ActiveRecord::Migration
+  def change
+    add_index :accounts, :facility_id
+    add_index :orders, :merge_with_order_id
+    add_index :orders, :account_id
+    add_index :product_users, :product_id
+    add_index :instrument_statuses, :instrument_id
+    add_index :products, :facility_id
+    add_index :products, :facility_account_id
+    add_index :account_users, :account_id
+    add_index :facility_accounts, :facility_id
+    add_index :price_policies, :price_group_id
+    add_index :versions, [:versioned_id, :versioned_type]
+    add_index :order_details, :product_accessory_id
+    add_index :order_details, :parent_order_detail_id
+    add_index :order_details, :product_id
+    add_index :order_details, :price_policy_id
+    add_index :order_details, :order_id
+    add_index :order_details, :account_id
+    add_index :order_details, :bundle_product_id
+    add_index :reservations, :order_detail_id
+    add_index :reservations, :product_id
+    add_index :price_group_members, :price_group_id
+    add_index :price_group_members, :account_id
+    add_index :statements, :facility_id
+    add_index :stored_files, :product_id
+    add_index :stored_files, :created_by
+    add_index :stored_files, :order_detail_id
+  end
+end
