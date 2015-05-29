0a005b9c057bde08a8f93911a988ba0f2c54069e
Lloyd Philbrook <lloyd@tablexi.com>
2015-05-29 13:09:58 +0800

    update oracle version and fix branch name

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,10 +1,10 @@
 set :deploy_to, "/home/nucore/nucore-test.northwestern.edu"
 set :rails_env, "stage"
-set :branch, "94398_upgrades"
+set :branch, "develop"
 
 server "nucore01s.northwestern.edu", user: "nucore", roles: %w(web app db)
 
 set :default_env, {
-  "LD_LIBRARY_PATH" => "/usr/lib/oracle/11.2/client64/lib",
-  "ORACLE_HOME" => "/usr/lib/oracle/11.2/client64/lib"
+  "LD_LIBRARY_PATH" => "/usr/lib/oracle/12.1/client64/lib",
+  "ORACLE_HOME" => "/usr/lib/oracle/12.1/client64/lib"
 }
