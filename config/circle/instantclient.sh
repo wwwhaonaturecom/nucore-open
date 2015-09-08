d348e9fe25612e1078e5544a3f96545196ca86e8
Jason Hanggi <jason@tablexi.com>
2015-09-08 18:13:14 -0500

    Fix typo in oracle script.
    This was causing CI to fail because of an invalid option

--- config/circle/instantclient.sh
+++ config/circle/instantclient.sh
@@ -8,13 +8,13 @@ fi
 
 # Download deb files
 if [[ ! -f ~/instantclient_deb/oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64.deb ]]; then
-  wget --n0-verbose --auth-no-challenge --header='Accept:application/octet-stream' https://$GITHUB_ACCESS:@api.github.com/repos/tablexi/nucore-nu/releases/assets/795779 -O ~/instantclient_deb/oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64.deb
+  wget --no-verbose --auth-no-challenge --header='Accept:application/octet-stream' https://$GITHUB_ACCESS:@api.github.com/repos/tablexi/nucore-nu/releases/assets/795779 -O ~/instantclient_deb/oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64.deb
 fi
 if [[ ! -f ~/instantclient_deb/oracle-instantclient12.1-sdk-12.1.0.2.0-1.x86_64.deb ]]; then
-  wget --n0-verbose --auth-no-challenge --header='Accept:application/octet-stream' https://$GITHUB_ACCESS:@api.github.com/repos/tablexi/nucore-nu/releases/assets/795777 -O ~/instantclient_deb/oracle-instantclient12.1-sdk-12.1.0.2.0-1.x86_64.deb
+  wget --no-verbose --auth-no-challenge --header='Accept:application/octet-stream' https://$GITHUB_ACCESS:@api.github.com/repos/tablexi/nucore-nu/releases/assets/795777 -O ~/instantclient_deb/oracle-instantclient12.1-sdk-12.1.0.2.0-1.x86_64.deb
 fi
 if [[ ! -f ~/instantclient_deb/oracle-instantclient12.1-sqlplus-12.1.0.2.0-1.x86_64.deb ]]; then
-  wget --n0-verbose --auth-no-challenge --header='Accept:application/octet-stream' https://$GITHUB_ACCESS:@api.github.com/repos/tablexi/nucore-nu/releases/assets/795778 -O ~/instantclient_deb/oracle-instantclient12.1-sqlplus-12.1.0.2.0-1.x86_64.deb
+  wget --no-verbose --auth-no-challenge --header='Accept:application/octet-stream' https://$GITHUB_ACCESS:@api.github.com/repos/tablexi/nucore-nu/releases/assets/795778 -O ~/instantclient_deb/oracle-instantclient12.1-sqlplus-12.1.0.2.0-1.x86_64.deb
 fi
 
 # Install instantclients
