fbc704fb98be9e77bf8a8eb278ccd863354617a1
Jason Hanggi <jason@tablexi.com>
2015-09-18 19:20:00 -0500

    Add NU-specific cronjobs to whenever

--- config/schedule_custom.rb
+++ config/schedule_custom.rb
@@ -1,6 +1,20 @@
 # Add your fork-specific cron jobs here
 # This file is automatically included by schedule.rb
 
-# every 20.minutes do
-#   rake "something"
-# end
+every :day, at: '7:20am' do
+  rake 'nucs:import:all[$HOME/files/FFRA-in/current]'
+end
+
+every :day, at: '7:40am' do
+  rake 'chart_strings:update_expiration'
+end
+
+if @environment == "production"
+  every :day, at: '5:15pm' do
+    rake 'jxml:render_and_move[$HOME/nucore.northwestern.edu/shared/journals,$HOME/files/FFRA-out/current]'
+  end
+end
+
+every :day, at: '7:00am' do
+  rake 'pmu:import'
+end
