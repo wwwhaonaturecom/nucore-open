6bc9b8d0c12e5883458e53a0352da97723186e36
Jason Hanggi <jason@tablexi.com>
2016-05-25 22:20:11 -0500

    Use Docker for Oracle image on circle
    This cuts down the time on CI from ~20 minutes to ~15. We'll also be able to kill off
    our RDS instance that we use solely for testing NUcore.

--- config/circle/database_create.sh
+++ config/circle/database_create.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 # Generate test database.yml file on circle ci
 
-DB_USERNAME="circle_${CIRCLE_BUILD_NUM}_${CIRCLE_NODE_INDEX}"
-
 # Generate user
-sqlplus "$DB_USER/$DB_PASSWORD@$DB_NAME" <<EOF
-GRANT CONNECT, RESOURCE TO $DB_USERNAME IDENTIFIED BY $DB_USERNAME;
-ALTER USER $DB_USERNAME DEFAULT TABLESPACE bc_nucore TEMPORARY TABLESPACE temp;
+sqlplus "system/oracle@localhost:1521/xe" <<EOF
+GRANT CONNECT, RESOURCE TO nucore_nu_test IDENTIFIED BY password;
+CREATE TABLESPACE BC_NUCORE DATAFILE 'bc_nucore.dat' SIZE 100M AUTOEXTEND ON;
+ALTER USER nucore_nu_test DEFAULT TABLESPACE bc_nucore TEMPORARY TABLESPACE temp;
 EXIT
 EOF
 
-cat <<EOF > config/database.yml
-test:
-  adapter: oracle_enhanced
-  database: '$DB_NAME'
-  username: $DB_USERNAME
-  password: $DB_USERNAME
-  encoding: utf8
-EOF
