108d475e9fd834d06747dbfdbc74ecb3f0a6502d
meara <meara@users.noreply.github.com>
2017-04-14 12:19:54 -0400

    [#135084] user nav bounceback (#242)
    * fix 404
    
    * fix user nav for user certificates
    
    * tweak

--- vendor/engines/nu_research_safety/app/controllers/nu_research_safety/user_certificates_controller.rb
+++ vendor/engines/nu_research_safety/app/controllers/nu_research_safety/user_certificates_controller.rb
@@ -21,13 +21,24 @@ module NuResearchSafety
 
     def check_certificate
       @user = User.find(params[:user_id])
-      @certificate = Certificate.find(params[:certificate_id])
-      if NuResearchSafety::CertificateApi.certified?(@user.username, @certificate.name)
-        flash[:notice] = text('check_certificate.success', user_name: @user.full_name, certificate_name: @certificate.name)
+      @certificate = Certificate.find_by(id: params[:certificate_id])
+
+      if @certificate
+        if NuResearchSafety::CertificateApi.certified?(@user.username, @certificate.name)
+          flash[:notice] = text('check_certificate.success', user_name: @user.full_name, certificate_name: @certificate.name)
+        else
+          flash[:error] = text('check_certificate.error', user_name: @user.full_name, certificate_name: @certificate.name)
+        end
       else
-        flash[:error] = text('check_certificate.error', user_name: @user.full_name, certificate_name: @certificate.name)
+        flash[:error] = text('check_certificate.none')
       end
+
       redirect_to action: :index
+    end
+
+    # During rendering, we want to use the Ability as if we were using the UsersController
+    def current_ability
+      ::Ability.new(current_user, current_facility, UsersController.new)
     end
 
   end
