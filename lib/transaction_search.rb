0e379b5ce971f7e69d45e41e601d5154e6a3777a
Jason Hanggi <jason@tablexi.com>
2017-06-05 18:59:23 -0500

    start refactoring search

--- lib/transaction_search.rb
+++ lib/transaction_search.rb
@@ -15,6 +15,45 @@ module TransactionSearch
     }
   end
 
+  class Searcher
+    attr_reader :date_range_field
+    def initialize(params, facility: nil, account: nil)
+      @order_details = OrderDetail.purchased
+      @order_details = @order_details.for_facility(facility) if facility
+      @order_details = @order_details.where(account_id: account.id) if account
+
+      @params = params
+    end
+
+    def order_details
+      order_details = @order_details
+                        .includes(:reservation)
+                        .includes(order: :user)
+                        .includes(:price_policy)
+                        .preload(:bundle)
+
+      order_details = TransactionSearch.searchers.reduce(order_details) do |ods, (key, searcher_class)|
+        searcher_class.new(ods).optimized
+      end
+
+      TransactionSearch.searchers.reduce(order_details) do |ods, (key, searcher_class)|
+        searcher = searcher_class.new(ods)
+        results = searcher.search(@params[key])
+        @date_range_field = searcher.date_range_field if key == :date_range
+        results
+      end
+    end
+
+    def search_options
+      order_details = @order_details.reorder(nil)
+
+      @search_options = TransactionSearch.searchers.map do |key, searcher|
+        [key, searcher.new(order_details).options]
+      end.to_h
+    end
+
+  end
+
   module ClassMethods
 
     # If a method is tagged with _with_search at the end, then define the normal controller
@@ -32,13 +71,13 @@ module TransactionSearch
 
     def define_search_method(new_method_name, old_method_name)
       define_method(new_method_name) do
-        init_order_details
+        @searcher = Searcher.new(params, facility: current_facility, account: @account)
         send(:"#{old_method_name}")
-        load_search_options
-        @empty_orders = @order_details.empty?
+        @search_options = @searcher.search_options
         @search_fields = params
-        do_search(@search_fields)
-        add_optimizations
+        @order_details = @searcher.order_details
+        @date_range_field = @searcher.date_range_field
+
         @paginate_order_details = false if request.format.csv?
         sort_and_paginate
         respond_to do |format|
@@ -56,26 +95,6 @@ module TransactionSearch
       field = "COALESCE(journal_date, in_range_statements.created_at)"
     end
     @order_details.order_by_desc_nulls_first(field)
-  end
-
-  def init_order_details
-    @order_details = OrderDetail.purchased
-
-    if current_facility
-      @order_details = @order_details.for_facility(current_facility)
-    end
-
-    @order_details = @order_details.where(account_id: @account.id) if @account
-  end
-
-  # Find all the unique search options based on @order_details. This needs to happen before do_search so these
-  # variables have the full non-searched section of values
-  def load_search_options
-    order_details = @order_details.reorder(nil)
-
-    @search_options = TransactionSearch.searchers.map do |key, searcher|
-      [key, searcher.new(order_details).options]
-    end.to_h
   end
 
   def paginate_order_details(per_page = nil)
@@ -96,35 +115,10 @@ module TransactionSearch
 
   private
 
-  def do_search(search_params)
-    # Rails.logger.debug "search: #{search_params}"
-    @order_details ||= OrderDetail.joins(:order).ordered
-    TransactionSearch.searchers.each do |key, searcher_class|
-      searcher = searcher_class.new(@order_details)
-      @order_details = searcher.search(search_params[key])
-      @date_range_field = searcher.date_range_field if key == :date_range
-    end
-  end
-
-  def add_optimizations
-    # cut down on some n+1s
-    @order_details = @order_details
-                     .includes(:reservation)
-                     .includes(order: :user)
-                     .includes(:price_policy)
-                     .preload(:bundle)
-
-    TransactionSearch.searchers.each do |_key, searcher|
-      @order_details = searcher.new(@order_details).optimized
-    end
-  end
-
   def sort_and_paginate
     @order_details = @order_details_sort ? @order_details.reorder(@order_details_sort) : order_by_desc
     @order_details = @order_details.paginate(pagination_args) if @paginate_order_details
   end
-
-  private
 
   def pagination_args
     args = { page: params[:page] }
